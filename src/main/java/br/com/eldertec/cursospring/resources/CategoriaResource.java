a630ca362e882a7efabfa7f4784401b3b0e026ef
eldertec <eldertec@gmail.com>
2018-08-10 20:26:04 -0300

    Inserindo dominio

--- src/main/java/br/com/eldertec/cursospring/resources/CategoriaResource.java
+++ src/main/java/br/com/eldertec/cursospring/resources/CategoriaResource.java
@@ -1,15 +1,29 @@
 package br.com.eldertec.cursospring.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.eldertec.cursospring.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "O REST FUNFOU!!!";
+	public List<Categoria> listar() {
+		
+		Categoria cat = new Categoria(1, "Informatica");
+		Categoria cat1 = new Categoria(2, "Escritorio");
+		
+		List<Categoria> retorno = new ArrayList<>();
+		
+		retorno.add(cat);
+		retorno.add(cat1);
+		
+		return retorno;
 	}
 }

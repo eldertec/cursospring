369687a08141a7ca6d8e4efbd5dec1bfea9ab8b5
eldertec <eldertec@gmail.com>
2018-08-21 20:46:31 -0300

    Criado a operação de instanciação

--- src/main/java/br/com/eldertec/cursospring/CursospringApplication.java
+++ src/main/java/br/com/eldertec/cursospring/CursospringApplication.java
@@ -1,12 +1,32 @@
 package br.com.eldertec.cursospring;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.com.eldertec.cursospring.domain.Categoria;
+import br.com.eldertec.cursospring.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class CursospringApplication {
+public class CursospringApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursospringApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+
 	}
 }

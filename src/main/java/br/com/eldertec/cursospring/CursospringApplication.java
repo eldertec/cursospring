07c29d1cf3720fa435572aba38f313979d01ffdb
eldertec <eldertec@gmail.com>
2018-09-11 15:48:38 -0300

    Pedido, EstadoPagamento e Pagamento

--- src/main/java/br/com/eldertec/cursospring/CursospringApplication.java
+++ src/main/java/br/com/eldertec/cursospring/CursospringApplication.java
@@ -1,5 +1,6 @@
 package br.com.eldertec.cursospring;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,13 +13,20 @@ import br.com.eldertec.cursospring.domain.Cidade;
 import br.com.eldertec.cursospring.domain.Cliente;
 import br.com.eldertec.cursospring.domain.Endereco;
 import br.com.eldertec.cursospring.domain.Estado;
+import br.com.eldertec.cursospring.domain.Pagamento;
+import br.com.eldertec.cursospring.domain.PagamentoComBoleto;
+import br.com.eldertec.cursospring.domain.PagamentoComCartao;
+import br.com.eldertec.cursospring.domain.Pedido;
 import br.com.eldertec.cursospring.domain.Produto;
+import br.com.eldertec.cursospring.domain.enums.EstadoPagamento;
 import br.com.eldertec.cursospring.domain.enums.TipoCliente;
 import br.com.eldertec.cursospring.repositories.CategoriaRepository;
 import br.com.eldertec.cursospring.repositories.CidadeRepository;
 import br.com.eldertec.cursospring.repositories.ClienteRepository;
 import br.com.eldertec.cursospring.repositories.EnderecoRepository;
 import br.com.eldertec.cursospring.repositories.EstadoRepository;
+import br.com.eldertec.cursospring.repositories.PagamentoRepository;
+import br.com.eldertec.cursospring.repositories.PedidoRepository;
 import br.com.eldertec.cursospring.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -36,6 +44,10 @@ public class CursospringApplication implements CommandLineRunner {
 	private ClienteRepository clienteRepository;
 	@Autowired
 	private EnderecoRepository enderecoRepository;
+	@Autowired
+	private PedidoRepository pedidoRepository;
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursospringApplication.class, args);
@@ -85,6 +97,23 @@ public class CursospringApplication implements CommandLineRunner {
 
 		clienteRepository.saveAll(Arrays.asList(cli1));
 		enderecoRepository.saveAll(Arrays.asList(e1, e2));
+
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+
+		Pedido ped1 = new Pedido(null, sdf.parse("30/09/2017 10:32"), cli1, e1);
+		Pedido ped2 = new Pedido(null, sdf.parse("10/10/2017 19:35"), cli1, e2);
+
+		Pagamento pgto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
+		ped1.setPagamento(pgto1);
+
+		Pagamento pgto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2017 00:00"),
+				null);
+		ped2.setPagamento(pgto2);
+
+		cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
+
+		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
+		pagamentoRepository.saveAll(Arrays.asList(pgto1, pgto2));
 
 	}
 }

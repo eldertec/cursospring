dccbd734929afdb6fb8e87892a11e2fb090a7e78
eldertec <eldertec@gmail.com>
2018-09-25 18:28:23 -0300

    PUT, DELETE e GET para Cliente

--- src/main/java/br/com/eldertec/cursospring/services/ClienteService.java
+++ src/main/java/br/com/eldertec/cursospring/services/ClienteService.java
@@ -1,12 +1,19 @@
 package br.com.eldertec.cursospring.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import br.com.eldertec.cursospring.domain.Cliente;
+import br.com.eldertec.cursospring.dto.ClienteDTO;
 import br.com.eldertec.cursospring.repositories.ClienteRepository;
+import br.com.eldertec.cursospring.services.exceptions.DataIntegrityException;
 import br.com.eldertec.cursospring.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -20,4 +27,39 @@ public class ClienteService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
 	}
+	
+	public Cliente update(Cliente obj) {
+		Cliente newObj = find(obj.getId());
+		updateData(newObj,obj);
+		return repo.save(newObj);
+	}
+
+	private void updateData(Cliente newObj, Cliente obj) {
+		newObj.setNome(obj.getNome());
+		newObj.setEmail(obj.getEmail());
+		
+	}
+
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("falta essa msg");
+		}
+	}
+	
+	public List<Cliente> findAll(){
+		return repo.findAll();
+	}
+	
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String direction, String orderBy){
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage,Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
+	}
+	
+	public Cliente fromDto(ClienteDTO objDto) {
+		return new Cliente(objDto.getId(),objDto.getNome(),objDto.getEmail(),null,null);
+	}
+
 }

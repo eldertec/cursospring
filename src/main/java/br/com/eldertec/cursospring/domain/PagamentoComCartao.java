89236d1a53f79639f519786d92edba01a4d72a2c
eldertec <eldertec@gmail.com>
2018-10-11 12:40:47 -0300

    Inserindo Pedido

--- src/main/java/br/com/eldertec/cursospring/domain/PagamentoComCartao.java
+++ src/main/java/br/com/eldertec/cursospring/domain/PagamentoComCartao.java
@@ -2,9 +2,12 @@ package br.com.eldertec.cursospring.domain;
 
 import javax.persistence.Entity;
 
+import com.fasterxml.jackson.annotation.JsonTypeName;
+
 import br.com.eldertec.cursospring.domain.enums.EstadoPagamento;
 
 @Entity
+@JsonTypeName("pagamentoComCartao")
 public class PagamentoComCartao extends Pagamento {
 	private static final long serialVersionUID = 1L;
 	private Integer numeroDeParcelas;
